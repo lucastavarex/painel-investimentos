de1ef2946827534e0e7baa0e4e58c6085412b2d4
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2023-08-18 12:59:50 -0300

    requisição para api do youtube para o vídeo + ajustes

--- app.js
+++ app.js
@@ -40,20 +40,34 @@ document.addEventListener("DOMContentLoaded", function() {
   var playButton = document.getElementById("play-button");
   var overlay = document.getElementById("overlay");
   var closeButton = document.getElementById("close-button");
-  var videoIframe = document.querySelector(".instagram-media iframe"); // Seleciona o elemento de vídeo do Instagram
+  var player;
 
   playButton.addEventListener("click", function() {
     overlay.style.display = "flex";
+
+    // Criar o player do YouTube quando o botão é clicado
+    if (!player) {
+      player = new YT.Player("player", {
+        videoId: "tXxTIJKBf30",  // ID do vídeo do YouTube
+        playerVars: {
+          controls: 0,  // Esconder os controles padrão do YouTube
+          autoplay: 1   // Iniciar a reprodução automaticamente
+        },
+        width: 800,      // Largura do vídeo em pixels
+        height: 450      // Altura do vídeo em pixels
+      });
+    }
   });
 
   closeButton.addEventListener("click", function() {
     overlay.style.display = "none";
     
-    // Pausa o vídeo do Instagram ao fechar o pop-up
-    if (videoIframe) {
-      videoIframe.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+    // Pausar o vídeo quando o modal é fechado
+    if (player) {
+      player.pauseVideo();
     }
   });
+
 });
 
 
