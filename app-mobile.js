550ecce4469954d825e6d9dc54f0edf335beea23
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2023-09-05 14:46:56 -0300

    .

--- app-mobile.js
+++ app-mobile.js
@@ -1,34 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
     var playButton = document.getElementById("play-button");
+    var playButton2 = document.getElementById("play-button-2");
     var overlay = document.getElementById("overlay");
     var closeButton = document.getElementById("close-button");
     var player;
+    var player2;
   
-    playButton.addEventListener("click", function () {
+    function openVideo(videoId) {
       overlay.style.display = "flex";
   
-      // Criar o player do YouTube quando o botão é clicado
       if (!player) {
         player = new YT.Player("player-container", {
-          videoId: "tXxTIJKBf30",  // ID do vídeo do YouTube
+          videoId: videoId,
           playerVars: {
-            controls: 1,  // Esconder os controles padrão do YouTube
-            autoplay: 1   // Iniciar a reprodução automaticamente
+            controls: 1,
+            autoplay: 1
           },
-          width: 1000,      // Largura do vídeo em pixels
-          height: 700      // Altura do vídeo em pixels
+          width: 1000,
+          height: 700
         });
+      } else {
+        player.loadVideoById(videoId); // Carrega o vídeo pelo ID
       }
+    }
+  
+    playButton.addEventListener("click", function () {
+      openVideo("tXxTIJKBf30"); // ID do primeiro vídeo
+    });
+  
+    playButton2.addEventListener("click", function () {
+      openVideo("tXxTIJKBf30"); // ID do segundo vídeo
     });
   
     closeButton.addEventListener("click", function () {
-        overlay.style.display = "none";
-      
-        // Pausar o vídeo quando o modal é fechado
-        if (player) {
-          player.stopVideo();
-        }
-      });
-      
+      overlay.style.display = "none";
+  
+      if (player) {
+        player.stopVideo();
+      }
+      if (player2) {
+        player2.stopVideo();
+      }
+    });
   });
- 
+  
